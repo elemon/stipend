69e5e070ea0234b2004bfd54b4228b0dade7f462
Eugene Lemon <eugenelemon@Eugenes-Mini.attlocal.net>
2022-02-01 21:32:55 -0800

    Final Working Stipend Tkinter Database

---
+++ Stipend_Total.sql
@@ -0,0 +1,10 @@
+select 
+	students.id as "Student#",
+	name as "Genius",
+	sum(credit) as "Credit",
+	sum(debit) as "Debit",
+	sum(credit-debit) as "Total Due" 
+		from stipends join  
+			students on students.id = student_id 
+				group by students.id 
+					order by name; 
